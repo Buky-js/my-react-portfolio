2bbed60e1ffe7d53d8e1f9725418bb0c7caf6bcc
Olubukola Abiona <bukyabiona@gmail.com>
2023-04-20 23:23:15 -0300

    added my third project - eventflow

--- src/assets/projects.js
+++ src/assets/projects.js
@@ -3,11 +3,19 @@ import fifa from "./images/fifa-world-cup.jpg";
 import weather from "./images/weather-dashboard.png";
 import quiz from "./images/timed-quiz.png";
 import pwdGen from "./images/pwd-gen.png";
-import codeEditor from "./images/code-editor.jpg";
+import eventPlanner from "./images/event-planner.jpg";
 
 const projects = [
     {
         id: 1,
+        name: 'Event-flow',
+        src: eventPlanner,
+        alt: 'event organisng app',
+        git: 'https://github.com/Buky-js/Event-Flow',
+        deployed: 'https://project-eventflow.herokuapp.com/'  
+    },
+    {
+        id: 2,
         name: 'Event-Time',
         src: events,
         alt: 'Event-time app',
@@ -15,7 +23,7 @@ const projects = [
         deployed: 'https://aqueous-plateau-28230.herokuapp.com'
     },
     {
-        id: 2,
+        id: 3,
         name: 'FIFA-Fever',
         src: fifa,
         alt: 'fifa-fever app',
@@ -23,7 +31,7 @@ const projects = [
         deployed: 'https://buky-js.github.io/FIFA-Fever/'  
     },
     {
-        id: 3,
+        id: 4,
         name: 'Weather Dashboard',
         src: weather,
         alt: 'weather-dashboard app',
@@ -31,7 +39,7 @@ const projects = [
         deployed: 'https://buky-js.github.io/Weather-Dashboard/'  
     },
     {
-        id: 4,
+        id: 5,
         name: 'Timed Quiz',
         src: quiz,
         alt: 'coding quiz app',
@@ -39,21 +47,14 @@ const projects = [
         deployed: 'https://buky-js.github.io/Timed-Quiz/'  
     },
     {
-        id: 5,
+        id: 6,
         name: 'Password Generator',
         src: pwdGen,
         alt: 'password-generator app',
         git: 'https://github.com/Buky-js/Password-Generator',
         deployed: 'https://buky-js.github.io/Password-Generator/'  
-    },
-    {
-        id: 6,
-        name: 'Text Editor',
-        src: codeEditor,
-        alt: 'text-editor app',
-        git: 'https://github.com/Buky-js/PWA---Text-Editorr',
-        deployed: 'https://damp-everglades-55411.herokuapp.com/'  
     }
+    
 ]
 
 export default projects;
